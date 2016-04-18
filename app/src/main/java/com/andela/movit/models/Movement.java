075179148819c77cc9066a5c59eb5b57f513449c
Eston Karumbi <eston.karumbi@andela.com>
2016-04-18 09:18:11 +0300

    Create Movement class

---
+++ app/src/main/java/com/andela/movit/models/Movement.java
@@ -0,0 +1,45 @@
+package com.andela.movit.models;
+
+
+public class Movement {
+
+    private String placeName;
+
+    private String activityName;
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    private double latitude;
+
+    private double longitude;
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
