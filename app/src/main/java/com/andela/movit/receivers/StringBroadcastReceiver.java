08579e1e1537aa6e78c9d364cfd3d9f72438c35c
Eston Karumbi <eston.karumbi@andela.com>
2016-04-24 18:47:38 +0300

    Refactor StringBroadcastReceiver

--- app/src/main/java/com/andela/movit/receivers/StringBroadcastReceiver.java
+++ app/src/main/java/com/andela/movit/receivers/StringBroadcastReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.andela.movit.config.Constants;
 import com.andela.movit.listeners.IncomingStringCallback;
 
 public class StringBroadcastReceiver extends BroadcastReceiver {
@@ -13,13 +12,17 @@ public class StringBroadcastReceiver extends BroadcastReceiver {
 
     private String actionName;
 
+    public StringBroadcastReceiver(String actionName) {
+        this.actionName = actionName;
+    }
+
     public void setIncomingStringCallback(IncomingStringCallback callback) {
         this.callback = callback;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        String activityName = intent.getStringExtra(Constants.ACTIVITY_NAME.getValue());
-        callback.onStringArrive(activityName);
+        String expectedString = intent.getStringExtra(actionName);
+        callback.onStringArrive(expectedString);
     }
 }
