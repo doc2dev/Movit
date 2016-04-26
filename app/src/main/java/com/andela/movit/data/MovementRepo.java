9b155b1f32555dd0098cdec02efbb179e98b563b
Eston Karumbi <eston.karumbi@andela.com>
2016-04-26 08:21:05 +0300

    Add fetch methods to MovementRepo

--- app/src/main/java/com/andela/movit/data/MovementRepo.java
+++ app/src/main/java/com/andela/movit/data/MovementRepo.java
@@ -2,10 +2,17 @@ package com.andela.movit.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.andela.movit.models.Movement;
+import com.andela.movit.utilities.Utility;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class MovementRepo extends SQLiteOpenHelper {
 
@@ -36,6 +43,12 @@ public class MovementRepo extends SQLiteOpenHelper {
             + LATITUDE + " FLOAT, "
             + LONGITUDE + " FLOAT);";
 
+    private static final String GET_BY_DATE = "SELECT * FROM "
+            + TABLE + " WHERE "
+            + TIMESTAMP + " > foo AND "
+            + TIMESTAMP + " < bar "
+            + "ORDER BY " + TIMESTAMP + " DESC;";
+
     public MovementRepo(Context context) {
         super(context, DATABASE, null, VERSION);
     }
@@ -47,15 +60,60 @@ public class MovementRepo extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
     }
 
     public long addMovement(Movement movement) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = getWriteContents(movement);
         long newRowId = db.insert(TABLE, null, values);
- //       db.close();
         return newRowId;
+    }
+
+    public List<Movement> getMovementsByDate(Date date) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] ranges = getDateRanges(date);
+        String query = GET_BY_DATE.replace("foo", ranges[0]).replace("bar", ranges[1]);
+        Cursor cursor = db.rawQuery(query, null);
+        return extractMovementsFromCursor(cursor);
+    }
+
+    public List<Movement> getAll() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE + ";";
+        Cursor cursor = db.rawQuery(query, null);
+        return extractMovementsFromCursor(cursor);
+    }
+
+    private List<Movement> extractMovementsFromCursor(Cursor cursor) {
+        List<Movement> movements = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            movements.add(getMovementFromCursor(cursor));
+        }
+        cursor.close();
+        return movements;
+    }
+
+    private Movement getMovementFromCursor(Cursor cursor) {
+        Movement movement = new Movement();
+        movement.setActivityName(cursor.getString(1));
+        movement.setPlaceName(cursor.getString(2));
+        movement.setTimeStamp(cursor.getLong(3));
+        movement.setLatitude(cursor.getDouble(4));
+        movement.setLongitude(cursor.getDouble(5));
+        return movement;
+    }
+
+    private String[] getDateRanges(Date date) {
+        int[] dateValues = Utility.getDateValues(date);
+        int year = dateValues[0];
+        int month = dateValues[1];
+        int day = dateValues[2];
+        Date date2 = Utility.generateDate(year, month, day - 1);
+        return new String[] {dateAsLongString(date2), dateAsLongString(date)};
+    }
+
+    private String dateAsLongString(Date date) {
+        return Long.toString(date.getTime());
     }
 
     private ContentValues getWriteContents(Movement movement) {
