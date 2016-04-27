1aac27a28d2c24e7b11a073ad57dd78114d4f133
Eston Karumbi <eston.karumbi@andela.com>
2016-04-27 10:22:16 +0300

    Implement VisitDetails view

---
+++ app/src/main/java/com/andela/movit/activities/VisitDetailsActivity.java
@@ -0,0 +1,29 @@
+package com.andela.movit.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.andela.movit.R;
+
+public class VisitDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_details);
+        prepareToolbar();
+    }
+
+    private void prepareToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
