0d4431424849c5d279aff15907d104f81e1ec3fc
Eston Karumbi <eston.karumbi@andela.com>
2016-04-27 20:30:26 +0300

    Implement settings storage in PreferenceHelper

--- app/src/main/java/com/andela/movit/utilities/PreferenceHelper.java
+++ app/src/main/java/com/andela/movit/utilities/PreferenceHelper.java
@@ -1,8 +1,32 @@
 package com.andela.movit.utilities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.andela.movit.config.Constants;
+
 public class PreferenceHelper {
 
-    public static long getTimeBeforeLogging() {
-        return 15000;
+    private Context context;
+
+    SharedPreferences preferences;
+
+    SharedPreferences.Editor editor;
+
+    public PreferenceHelper(Context context) {
+        this.context = context;
+        preferences= PreferenceManager.getDefaultSharedPreferences(context);
+        editor = preferences.edit();
+    }
+
+    public long getTimeBeforeLogging() {
+        int minutes = preferences.getInt(Constants.TIME_BEFORE_LOGGING.getValue(), 1);
+        return minutes * 60000;
+    }
+
+    public void setTimeBeforeLogging(int timeBeforeLogging) {
+        editor.putInt(Constants.TIME_BEFORE_LOGGING.getValue(), timeBeforeLogging);
+        editor.commit();
     }
 }
