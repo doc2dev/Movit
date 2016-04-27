ef7dbcf99f47f312b8f8914fe20219ccd6225efe
Eston Karumbi <eston.karumbi@andela.com>
2016-04-27 10:02:51 +0300

    Create FillableAdapter class

---
+++ app/src/main/java/com/andela/movit/adapters/FillableAdapter.java
@@ -0,0 +1,29 @@
+package com.andela.movit.adapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public abstract class FillableAdapter<T> extends ArrayAdapter<T> {
+
+    protected Context context;
+
+    protected List<T> items;
+
+    protected int viewId;
+
+    public FillableAdapter(Context context, int resource, List<T> objects) {
+        super(context, resource, objects);
+        this.items = objects;
+        this.context = context;
+        this.viewId = resource;
+    }
+
+    public void fillItems(List<T> incomingItems) {
+        items.clear();
+        for (T item : incomingItems) {
+            items.add(item);
+        }
+    }
+}
