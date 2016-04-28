c80df12bc8224c6992a247dfc618fe2a691e3100
Eston Karumbi <eston.karumbi@andela.com>
2016-04-28 16:13:58 +0300

    Add javadoc

--- app/src/main/java/com/andela/movit/background/TimerTickListener.java
+++ app/src/main/java/com/andela/movit/background/TimerTickListener.java
@@ -1,5 +1,15 @@
+/**
+ * This interface provides a callback to be invoked whenever a {@code CountUpTimer} timer ticks.
+ * */
+
 package com.andela.movit.background;
 
 public interface TimerTickListener {
+
+    /**
+     * Called whenever the timer ticks.
+     * @param elapsedTime the time (in milliseconds) since the timer was started.
+     * */
+
     void onTick(long elapsedTime);
 }
