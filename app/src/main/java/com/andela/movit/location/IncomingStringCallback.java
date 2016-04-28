e9011f8cce36a5089d852bf44f706c30843e783f
Eston Karumbi <eston.karumbi@andela.com>
2016-04-28 16:21:09 +0300

    Refactor and add javadoc

--- app/src/main/java/com/andela/movit/location/IncomingStringCallback.java
+++ app/src/main/java/com/andela/movit/location/IncomingStringCallback.java
@@ -1,3 +1,8 @@
+/**
+ * This interface defines an operation to be performed whenever a string value that was expected
+ * from a given source in the future (e.g. a database, broadcast, network fetch) has arrived.
+ * */
+
 package com.andela.movit.location;
 
 public interface IncomingStringCallback {
