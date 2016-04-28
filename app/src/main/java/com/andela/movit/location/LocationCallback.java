0d0f20ca62cf850dba4b4a885667b04bf72670d0
Eston Karumbi <eston.karumbi@andela.com>
2016-04-28 16:22:04 +0300

    Add javadoc

--- app/src/main/java/com/andela/movit/location/LocationCallback.java
+++ app/src/main/java/com/andela/movit/location/LocationCallback.java
@@ -1,3 +1,7 @@
+/**
+ * This interface defines an operation to be performed whenever a new location is detected.
+ * */
+
 package com.andela.movit.location;
 
 import com.andela.movit.models.Movement;
